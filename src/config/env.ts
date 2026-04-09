9ab183b36695432d68fa9c42364c25a09f439ca7
03102909 <perkovasofiia@gmail.com>
2026-04-09 21:14:03 +0300

    chore: initial project setup

---
+++ src/config/env.ts
@@ -0,0 +1,6 @@
+import 'dotenv/config';
+
+const PORT = process.env.PORT;
+const DATABASE_URL = process.env.DATABASE_URL;
+
+export default { PORT, DATABASE_URL };
---
+++ src/db/prisma.ts
@@ -0,0 +1,14 @@
+import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../generated/prisma/client.js';
+import env from '../config/env.js';
+
+const pool = new Pool({
+  connectionString: env.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
+
+const prisma = new PrismaClient({ adapter });
+
+export default prisma;
